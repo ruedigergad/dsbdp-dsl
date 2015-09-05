3c60e0a2c152dda3b3d425cbd3418ad05d20547f
Ruediger Gad <r.c.g@gmx.de>
2015-09-05 17:37:04 +0200

    Warn on reflection.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :global-vars {*warn-on-reflection* true}
   :java-source-paths ["src-java"]
   :javac-options     ["-target" "1.6" "-source" "1.6"])
