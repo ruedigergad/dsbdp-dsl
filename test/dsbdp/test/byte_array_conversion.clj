f8cf77280987d92848ed681ec1dd1bc48e7f30f4
Ruediger Gad <r.c.g@gmx.de>
2015-09-05 13:28:58 +0200

    Begin to add namespaces for tests and implementation of byte array conversions.

---
+++ test/dsbdp/test/byte_array_conversion.clj
@@ -0,0 +1,18 @@
+;;;
+;;;   Copyright 2015, Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Unit tests for byte array conversion"}
+  dsbdp.test.byte-array-conversion
+  (:require [clojure.test :refer :all]
+            [dsbdp.byte-array-conversion :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
