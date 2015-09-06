ce9202d42f0297c6e1e9c700c6f9fca105e4ce9b
Ruediger Gad <r.c.g@gmx.de>
2015-09-06 16:06:24 +0200

    Add first test and implementation for processing byte array data to Java map output.

--- src/dsbdp/data_processing_dsl.clj
+++ src/dsbdp/data_processing_dsl.clj
@@ -12,3 +12,60 @@
   dsbdp.data-processing-dsl
   (:require [dsbdp.byte-array-conversion :refer :all]))
 
+(defn create-data-processing-sub-fn
+  [data-processing-definition input offset]
+  (into
+    '()
+    (reverse
+      (reduce
+        (fn [v data-processing-element]
+          (cond
+            (or
+              (keyword? data-processing-element)
+              (symbol? data-processing-element)) (let [s (symbol (name data-processing-element))]
+                                                   (condp not= nil
+                                                     (ns-resolve 'clojure.core s)
+                                                       (conj v (ns-resolve 'clojure.core s))
+                                                     (ns-resolve 'dsbdp.byte-array-conversion s)
+                                                       (conj v (ns-resolve 'dsbdp.byte-array-conversion s) 'input)
+                                                     (do
+                                                       (println "Could not resolve keyword/symbol:" s)
+                                                        v)))
+            (list? data-processing-element) (conj v (into '() (reverse (create-data-processing-sub-fn data-processing-element input offset))))
+            :default (conj v data-processing-element)))
+        [] data-processing-definition))))
+
+(defn create-data-processing-fn-body-for-java-map-type
+  "Create a data processing function body for extracting data into a Java map."
+  [input offset rules]
+  (reduce
+    (fn [v rule]
+      (conj v `(.put
+                ~(name (first rule))
+                ~(create-data-processing-sub-fn (second rule) input offset))))
+    '[doto (java.util.HashMap.)] rules))
+
+(defn create-data-processing-fn
+  "Create a data processing function based on the given dsl-expression."
+  [dsl-expression]
+;  (println "Got DSL expression:" dsl-expression)
+  (let [input-sym 'input
+        offset-sym 'offset
+        fn-body-vec (let [rules (:rules dsl-expression)
+                          input-type (:input-type dsl-expression)
+                          output-type (:output-type dsl-expression)]
+                      (condp = (name output-type)
+                        "java-map" (create-data-processing-fn-body-for-java-map-type input-sym offset-sym rules)
+;                        "clj-map" (create-data-processing-fn-body-for-clj-map-type input-sym offset-sym rules)
+;                        "csv-str" (create-data-processing-fn-body-for-csv-str-type input-sym offset-sym rules)
+;                        "json-str" (create-data-processing-fn-body-for-json-str-type input-sym offset-sym rules)
+                        (do
+                          (println "Unknown output type:" output-type)
+                          (println "Defaulting to :java-maps as output type.")
+                          (create-data-processing-fn-body-for-java-map-type input-sym offset-sym rules))))
+;        _ (println "Created data processing function vector from DSL:" fn-body-vec)
+        fn-body (reverse (into '() fn-body-vec))
+;        _ (println "Created data processing function body:" fn-body)
+        data-processing-fn (eval `(fn [~input-sym ~offset-sym] ~fn-body))]
+    data-processing-fn))
+
--- test/dsbdp/test/data_processing_dsl_tests.clj
+++ test/dsbdp/test/data_processing_dsl_tests.clj
@@ -13,4 +13,23 @@
   (:require [clojure.test :refer :all]
             [dsbdp.data-processing-dsl :refer :all]))
 
+(def byte-array-test-data
+  "We use the byte array representation of a captured UDP packet as byte array test data."
+  (byte-array
+    (map byte [-5 -106 -57 84   15 -54 14 0   77 0 0 0   77 0 0 0    ; 16 byte pcap header
+               -1 -2 -3 -14 -15 -16 1 2 3 4 5 6 8 0                  ; 14 byte Ethernet header
+               69 0 0 32 0 3 64 0 7 17 115 -57 1 2 3 4 -4 -3 -2 -1   ; 20 byte IP header
+               8 0 16 0 0 4 -25 -26                                  ; 8 byte UDP header
+               97 98 99 100])))                                      ; 4 byte data "abcd"
+
+(deftest byte-array-to-java-map-test
+  (let [expected {"udpSrc" 2048, "udpDst" 4096}
+        dsl-expression {:input-type :byte-array
+                        :output-type :java-map
+                        :rules [['udpSrc '(int16 50)]
+                                ['udpDst '(int16 52)]]}
+        data-processing-fn (create-data-processing-fn dsl-expression)
+        result (data-processing-fn byte-array-test-data 0)]
+    (is (= java.util.HashMap (type result)))
+    (is (= expected result))))
 
