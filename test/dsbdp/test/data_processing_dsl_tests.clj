877aa1b7f3a515b22cb37a72e5780e3df3a66028
Ruediger Gad <r.c.g@gmx.de>
2015-09-06 15:36:52 +0200

    Add stubs for the data processing DSL.

---
+++ test/dsbdp/test/data_processing_dsl_tests.clj
@@ -0,0 +1,16 @@
+;;;
+;;;   Copyright 2015 Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Unit tests for data processing DSL"}
+  dsbdp.test.data-processing-dsl-tests
+  (:require [clojure.test :refer :all]
+            [dsbdp.data-processing-dsl :refer :all]))
+
+
