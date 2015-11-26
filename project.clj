7d79677bbb2ae2667ac0c1293fbf2cf83e9fc006
Ruediger Gad <r.c.g@gmx.de>
2015-11-26 22:56:17 +0100

    Add dependencies.

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-assorted-utils "1.11.0"]]
+                 [clj-assorted-utils "1.11.0"]
+                 [org.apache.commons/commons-math3 "3.5"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :global-vars {*warn-on-reflection* true}
   :java-source-paths ["src-java"]
   :javac-options     ["-target" "1.6" "-source" "1.6"]
