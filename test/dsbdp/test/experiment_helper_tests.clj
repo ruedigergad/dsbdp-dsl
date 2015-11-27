6c1ded2b58282c3381bbf92819baf0b47565b0ba
Ruediger Gad <r.c.g@gmx.de>
2015-11-27 07:43:14 +0100

    Add stubs for experiment-helper.

---
+++ test/dsbdp/test/experiment_helper_tests.clj
@@ -0,0 +1,14 @@
+;;;
+;;;   Copyright 2015 Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Unit tests for a helpers that are primarily used for experiments"}
+  dsbdp.test.experiment-helper-tests
+  (:require [clojure.test :refer :all]))
+
