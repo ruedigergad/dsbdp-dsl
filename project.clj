1f31081a342edb5518e330bac0dd308d3d9141cf
Ruediger Gad <r.c.g@gmx.de>
2015-11-28 20:47:20 +0100

    Further rework monitoring code.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-assorted-utils "1.11.0"]
+                 [clj-assorted-utils "1.11.1"]
                  [org.apache.commons/commons-math3 "3.5"]
                  [org.clojure/tools.cli "0.3.3"]]
   :global-vars {*warn-on-reflection* true}
