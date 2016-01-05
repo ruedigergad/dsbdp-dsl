559e9ed9494f081de7a47dafdba801a6c14cb47d
Ruediger Gad <r.c.g@gmx.de>
2016-01-05 22:38:55 +0100

    First version of factorial-direct.

--- src/dsbdp/main.clj
+++ src/dsbdp/main.clj
@@ -80,20 +80,24 @@
           scenario (:scenario options)
           in-data (condp (fn [^String v ^String s] (.startsWith s v)) scenario
                     "no-op" 1
-                    "factorial" 24N
+                    "factorial" 300N
                     "pcap" pcap-byte-array-test-data
                     "nil" nil
                     )
-          proc-fn (create-proc-fn sample-pcap-processing-definition-json)
+          proc-fn (condp = scenario
+                    "pcap-direct" (create-proc-fn sample-pcap-processing-definition-json)
+                    "factorial-direct" factorial)
           fn-mapping (:fn-mapping options)
           pipeline-length (:pipeline-length options)
           pipeline (if (and
                          (not (nil? in-data))
-                         (not= scenario "pcap-direct"))
+                         (not= scenario "pcap-direct")
+                         (not= scenario "factorial-direct"))
                      (create-local-processing-pipeline
                        (condp = scenario
                          "no-op" (create-no-op-proc-fns pipeline-length)
-                         "factorial" (create-factorial-proc-fns pipeline-length)
+                         "factorial" [(fn [i _] (factorial i))] 
+                         "factorial#inc" (create-factorial-proc-fns pipeline-length)
                          "pcap-json" [(fn [i _] (proc-fn i))]
                          "pcap-json#inc" (create-proc-fns-vec
                                            fn-mapping
@@ -107,9 +111,11 @@
           in-loop (ProcessingLoop.
                     "InLoop"
                     (cond
-                      (= "pcap-direct" scenario) (fn []
-                                                   (proc-fn in-data)
-                                                   (.inc out-cntr))
+                      (or
+                        (= "pcap-direct" scenario)
+                        (= "factorial-direct")) (fn []
+                                                  (proc-fn in-data)
+                                                  (.inc out-cntr))
                       (not (nil? in-data)) (fn []
                                              (doseq [i (repeat 1000 0)]
                                                (in-fn in-data)
