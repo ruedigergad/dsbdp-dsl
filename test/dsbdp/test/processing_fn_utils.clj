6cd9d9aef9143aac0b949864db78c8326fafe128
Ruediger Gad <r.c.g@gmx.de>
2016-01-19 08:23:10 +0100

    Rename var.

--- test/dsbdp/test/processing_fn_utils.clj
+++ test/dsbdp/test/processing_fn_utils.clj
@@ -42,18 +42,18 @@
 
 (deftest simple-combine-proc-fns-1st-test
   (let [proc-fns (create-proc-fn-vec-from-template '(fn [i _] (+ i :_idx_ 1)) '(fn [_ o] (+ o :_idx_ 1)) 6)
-        mapped-proc-fn (combine-proc-fns proc-fns 0 3)]
+        combined-proc-fn (combine-proc-fns proc-fns 0 3)]
     (is (= 1 ((proc-fns 0) 0 nil)))
     (is (= 2 ((proc-fns 1) nil 0)))
     (is (= 6 ((proc-fns 5) nil 0)))
-    (is (= 6 (mapped-proc-fn 0 nil)))
-    (is (= 10 (mapped-proc-fn 4 nil)))))
+    (is (= 6 (combined-proc-fn 0 nil)))
+    (is (= 10 (combined-proc-fn 4 nil)))))
 
 (deftest simple-combine-proc-fns-nth-test
   (let [proc-fns (create-proc-fn-vec-from-template '(fn [i _] (+ i :_idx_ 1)) '(fn [_ o] (+ o :_idx_ 1)) 6)
-        mapped-proc-fn (combine-proc-fns proc-fns 3 5)]
+        combined-proc-fn (combine-proc-fns proc-fns 3 5)]
     (is (= 4 ((proc-fns 3) nil 0)))
     (is (= 5 ((proc-fns 4) nil 0)))
-    (is (= 9 (mapped-proc-fn nil 0)))
-    (is (= 15 (mapped-proc-fn nil 6)))))
+    (is (= 9 (combined-proc-fn nil 0)))
+    (is (= 15 (combined-proc-fn nil 6)))))
 
