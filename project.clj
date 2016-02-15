7c29715bf3dbd490428e52518fd92c81b7b792f0
Ruediger Gad <r.c.g@gmx.de>
2016-02-15 19:26:55 +0100

    Update clojure version.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-assorted-utils "1.11.1"]
                  [org.apache.commons/commons-math3 "3.5"]
                  [org.clojure/tools.cli "0.3.3"]
