825535fbb254955c0a06ed2347b7559f86596221
Ruediger Gad <r.c.g@gmx.de>
2016-05-12 08:03:46 +0200

    Add test-ns and ns stubs for parallel processing functionality.

---
+++ src/dsbdp/parallel_processing.clj
@@ -0,0 +1,17 @@
+;;;
+;;;   Copyright 2016 Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Functionality for parallelized processing."}
+  dsbdp.parallel-processing
+  (:require
+    [dsbdp.local-data-processing-pipeline :refer :all]))
+
+(defn stub [])
+
---
+++ test/dsbdp/test/parallel_processing_tests.clj
@@ -0,0 +1,20 @@
+;;;
+;;;   Copyright 2016 Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Unit tests for parallelized data processing"}
+  dsbdp.test.parallel-processing-tests
+  (:require
+    [clojure.test :refer :all]
+    [clj-assorted-utils.util :refer :all]
+    [dsbdp.parallel-processing :refer :all]))
+
+(deftest stub-test
+  (stub))
+
