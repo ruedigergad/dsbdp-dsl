11121bf239ed98c68a853b1a389fee78f563726c
Ruediger Gad <r.c.g@gmx.de>
2017-04-12 09:29:43 +0200

    Add ICMP sample data.

--- src/dsbdp/experiment_helper.clj
+++ src/dsbdp/experiment_helper.clj
@@ -33,6 +33,20 @@
 ;              2048    4096
                97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112])))  ; 16 byte data "abcdefghijklmnop"
 
+(def pcap-icmp-byte-array-test-data
+  "The byte array representation of an ICMP packet for being used as dummy data."
+  (byte-array
+    (map byte [-5 -106 -57 84   15 -54 14 0          58 0 0 0   58 0 0 0                ; 16 byte pcap header
+;              1422366459969231000 (Unix Timestamp)
+               -1 -2 -3 -14 -15 -16    1 2 3 4 5 6          8 0                         ; 14 byte Ethernet header
+;              FF:FE:FD:F2:F1:F0       01:02:03:04:05:06
+               69 0 0 84   80 39 64 0   64 1 -127 -85   10 0 0 66   -43 -128 -119 20    ; IPv4 Header
+
+               8 0 32 -66   101 25 0 3   6 16 -21 88   0 0 0 0                          ; ICMP Header
+               ; ICMP Data
+               -63 -23 0 0 0 0 0 0 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55
+])))
+
 (def pcap-tcp-byte-array-test-data
   "The byte array representation of a TCP packet for being used as dummy data."
   (byte-array
