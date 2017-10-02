bbd5c467eba8cedd31dd1d4d3037974632afae3c
Ruediger Gad <r.c.g@gmx.de>
2017-10-02 20:53:42 +0200

    Revert to snapshot version.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;(defproject dsbdp "0.4.1-SNAPSHOT"
-(defproject dsbdp "0.4.0"
+(defproject dsbdp "0.4.1-SNAPSHOT"
+;(defproject dsbdp "0.4.0"
   :description "Dynamic Stream and Batch Data Processing (dsbdp)"
   :url "https://github.com/ruedigergad/dsbdp"
   :license {:name "Eclipse Public License"
