051bec84b86ca7fb17f22eaf57caca87690679b2
Ruediger Gad <r.c.g@gmx.de>
2018-08-28 21:51:10 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :url "https://github.com/ruedigergad/dsbdp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
-                 [clj-assorted-utils "1.17.1"]
-                 [org.clojure/tools.cli "0.3.3"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]
+                 [clj-assorted-utils "1.18.2"]
+                 [org.clojure/tools.cli "0.3.7"]
                  [commons-net "3.6"]]
   :global-vars {*warn-on-reflection* true}
   :java-source-paths ["src-java"]
 ;  :javac-options     ["-target" "1.6" "-source" "1.6"]
-  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.2.2" :exclusions  [org.clojure/clojure]]]}}
+  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.2.9" :exclusions  [org.clojure/clojure]]]}}
   :plugins [[lein-cloverage "1.0.6"]]
   :test2junit-output-dir "ghpages/test-results"
   :test2junit-run-ant true  
