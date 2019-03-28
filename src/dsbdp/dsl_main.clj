d72e61e45afaaf90ffc195ad96c5acc910e69967
Ruediger Gad <r.c.g@gmx.de>
2019-03-28 17:35:08 +0100

    Add test and implementation for proc-bin-file command.

--- src/dsbdp/dsl_main.clj
+++ src/dsbdp/dsl_main.clj
@@ -13,8 +13,12 @@
   (:require
     (cli4clj [cli :as cli])
     (clj-assorted-utils [util :as utils])
+    (clojure
+      [pprint :as pprint])
     (dsbdp
       [data-processing-dsl :as dsl]))
+  (:import
+    (java.nio.file Files Paths))
   (:gen-class))
 
 (defn -main [& args]
@@ -43,11 +47,23 @@
                                                 (.printStackTrace e)))
                                             (println "Error: cannot set processing function from invalid DSL expression:" (str n)))))]
     (cli/start-cli
-      {:cmds {:load-dsl {:fn (fn [dsl-file-name]
-                               (println "Loading DSL from:" dsl-file-name)
-                               (let [s (slurp (str dsl-file-name))]
-                                 (if (not (empty? s))
-                                   (reset! dsl-string s)
-                                   (println "Failed to read DSL from:" dsl-file-name))
-                                 nil))}}})))
+      {:cmds
+       {:load-dsl {:fn (fn [dsl-file-name]
+                         (println "Loading DSL from:" dsl-file-name)
+                         (let [s (slurp (str dsl-file-name))]
+                           (if (not (empty? s))
+                             (reset! dsl-string s)
+                             (println "Failed to read DSL from:" dsl-file-name))
+                           nil))}
+        :proc-bin-file {:fn (fn [bin-file-name]
+                              (if (utils/file-exists? (str bin-file-name))
+                                (try
+                                  (println "Processing file:" (str bin-file-name))
+                                  (let [in-data (Files/readAllBytes (Paths/get (str bin-file-name) (into-array [""])))
+                                        out-data (@proc-fn in-data)]
+                                    (pprint/pprint out-data))
+                                  (catch Exception e
+                                    (println "Error processing binary file:" (str bin-file-name))
+                                    (.printStackTrace e)))
+                                (println "Error file does not exits:" (str bin-file-name))))}}})))
 
--- test/dsbdp/test/dsl_main.clj
+++ test/dsbdp/test/dsl_main.clj
@@ -28,3 +28,57 @@
                   "Processing function set."])
                out-string))))
 
+(test/deftest process-binary-file
+  (let [test-cmd-input ["load-dsl test/data/pcap_file_example_dsl.txt"
+                        "proc-bin-file test/data/pcap_three_packets_icmp_dns_http.pcap"]
+        out-string (cli-tests/test-cli-stdout #(dsl-main/-main "") test-cmd-input)]
+    (test/is (=
+               (cli-tests/expected-string
+                 ["Loading DSL from: test/data/pcap_file_example_dsl.txt"
+                  "Setting DSL expression..."
+                  "Setting processing function..."
+                  "Processing function set."
+                  "Processing file: test/data/pcap_three_packets_icmp_dns_http.pcap"
+                  "{\"magic-number\" 2712847316,"
+                  " \"snapshot-len\" 262144,"
+                  " \"packets\""
+                  " [{\"capture-length\" 98,"
+                  "   \"packet-length\" 98,"
+                  "   \"__offset-increment\" 114,"
+                  "   \"data\""
+                  "   {\"eth-dst\" \"E8:DE:27:59:0E:AD\","
+                  "    \"eth-src\" \"0E:AD:B4:74:9F:FA\","
+                  "    \"data\""
+                  "    {\"proto-id\" 1,"
+                  "     \"ip-dst\" \"10.0.0.230\","
+                  "     \"ip-src\" \"149.20.4.69\","
+                  "     \"data\" {\"type\" \"Echo Request\"}}}}"
+                  "  {\"capture-length\" 70,"
+                  "   \"packet-length\" 70,"
+                  "   \"__offset-increment\" 86,"
+                  "   \"data\""
+                  "   {\"eth-dst\" \"E8:DE:27:59:0E:AD\","
+                  "    \"eth-src\" \"0E:AD:B4:74:9F:FA\","
+                  "    \"data\""
+                  "    {\"proto-id\" 17,"
+                  "     \"ip-dst\" \"10.0.0.230\","
+                  "     \"ip-src\" \"10.0.0.1\","
+                  "     \"data\" {\"dst\" 53, \"src\" 43471}}}}"
+                  "  {\"capture-length\" 74,"
+                  "   \"packet-length\" 74,"
+                  "   \"__offset-increment\" 90,"
+                  "   \"data\""
+                  "   {\"eth-dst\" \"E8:DE:27:59:0E:AD\","
+                  "    \"eth-src\" \"0E:AD:B4:74:9F:FA\","
+                  "    \"data\""
+                  "    {\"proto-id\" 6,"
+                  "     \"ip-dst\" \"10.0.0.230\","
+                  "     \"ip-src\" \"199.204.44.194\","
+                  "     \"data\""
+                  "     {\"dst\" 80,"
+                  "      \"src\" 49526,"
+                  "      \"flags\" 74,"
+                  "      \"ack-no\" 0,"
+                  "      \"seq-no\" 1034137073}}}}]}"])
+               out-string))))
+
