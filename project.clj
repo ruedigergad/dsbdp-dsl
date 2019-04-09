15d126000e0aa3e0caecc77e3fa85b212c1918f3
Ruediger Gad <r.c.g@gmx.de>
2019-04-09 20:44:13 +0200

    Try to support JDK >= 9.

--- project.clj
+++ project.clj
@@ -10,6 +10,14 @@
   :global-vars {*warn-on-reflection* true}
   :java-source-paths ["src-java"]
 ;  :javac-options     ["-target" "1.6" "-source" "1.6"]
+  :jvm-opts ~(let [java-major-version (->
+                                        (System/getProperty "java.version")
+                                        (clojure.string/split #"\.")
+                                        (first)
+                                        (java.lang.Integer/parseInt))]
+               (if (>= java-major-version 9)
+                 ["--add-modules" "java.xml.bind"]
+                 []))
   :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.3.5" :exclusions  [org.clojure/clojure]]]}}
   :plugins [[lein-cloverage "1.0.6"]]
   :test2junit-output-dir "ghpages/test-results"
